4b86bbba99c72f0f95c99702cf18eb1b300d66db
Caleb-LeGrand12 <c.mugisha@alustudent.com>
2021-05-03 08:55:32 +0200

    auth, db

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
 const app = express();
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || "3001";
+const port = process.env.PORT || "3011";
 
-app.get('/', (req, res)=>{
-    res.json({message: "Welcome to The WaterCo API"});
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to The WaterCo API" });
 });
 
-app.listen(port, ()=>{
-    console.log(`WaterCo API Running on Port ${port}`);
+app.listen(port, () => {
+  console.log(`WaterCo API Running on Port ${port}`);
 });
 
 export default app;
