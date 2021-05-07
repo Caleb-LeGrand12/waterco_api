4cb09f065e7a35ad79580d3a281aa410ec852308
Caleb-LeGrand12 <c.mugisha@alustudent.com>
2021-05-07 08:19:43 +0200

    en

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
